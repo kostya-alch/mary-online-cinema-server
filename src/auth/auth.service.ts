8c11cb994830e5b486546ce013eb459ae5c09021
kostya-alch <kostafrompiter@gmail.com>
2022-04-06 17:07:13 +0300

    logic auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import {
+   BadRequestException,
+   Injectable,
+   UnauthorizedException,
+} from '@nestjs/common';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { InjectModel } from 'nestjs-typegoose';
 import { UserModel } from 'src/user/model/user.model';
+import { genSalt, hash, compare } from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -9,8 +15,36 @@ export class AuthService {
       @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
    ) {}
 
-   async register(dto: any) {
-      const newUser = new this.UserModel(dto);
+   async login(dto: any) {
+      return this.validateUser(dto);
+   }
+
+   async register(dto: AuthDto) {
+      const oldUser = await this.UserModel.findOne({ email: dto.email }); // ищем конкретного юзера
+      if (oldUser) {
+         // если не нашли - бросаем ошибку
+         throw new BadRequestException(
+            'User with this email is already in the system'
+         );
+      }
+      const salt = await genSalt(10); // генерируем соль для хэша пароля
+      const newUser = new this.UserModel({
+         email: dto.email,
+         password: await hash(dto.password, salt),
+      }); // и создаем нового в бд
       return newUser.save();
+   }
+
+   async validateUser(dto: AuthDto): Promise<UserModel> {
+      // функция валидации юзера
+      const user = await this.UserModel.findOne({ email: dto.email });
+      if (!user) {
+         throw new UnauthorizedException('User not found!');
+      }
+      const isValidPassword = await compare(dto.password, user.password); // compare сверяет пароли
+      if (!isValidPassword) {
+         throw new UnauthorizedException('Invalid password!');
+      }
+      return user;
    }
 }
