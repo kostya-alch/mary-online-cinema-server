56e6638e941b2041a12fbcc1381c805032291813
kostya-alch <kostafrompiter@gmail.com>
2022-04-13 22:01:17 +0300

    aggregate for actor

--- src/actor/actor.service.ts
+++ src/actor/actor.service.ts
@@ -40,11 +40,22 @@ export class ActorService {
          };
       }
 
-      // TODO
-      return this.ActorModel.find(options)
-         .select('-updatedAt -__v')
+      return this.ActorModel.aggregate()
+         .match(options)
+         .lookup({
+            from: 'Movie',
+            localField: '_id',
+            foreignField: 'actors',
+            as: 'movies',
+         })
+         .addFields({
+            countMovies: {
+               $size: '$movies',
+            },
+         })
+         .project({ __v: 0, updatedAt: 0, movies: 0 })
          .sort({
-            createdAt: 'desc',
+            createdAt: -1,
          })
          .exec();
    }
